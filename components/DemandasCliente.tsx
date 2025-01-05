04facd0167f20b2a86a14c3a79cb742c1404a10d
Alan Hackbartt <123487644+Alan-H-H@users.noreply.github.com>
2025-01-05 02:27:42 -0300

    Update DemandasCliente.tsx

--- components/DemandasCliente.tsx
+++ components/DemandasCliente.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useEffect, useRef } from 'react';
 import ModalDetallesPago from '@/components/ModalDetallesPago';
 import { deleteDemanda, getDemandasByCategoria } from '@/actions/demanda-actions';
